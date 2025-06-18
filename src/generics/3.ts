9aaf5985028c1c96da400999320fc908cd1ba165
Valentyna <valia.melnik@gmail.com>
2025-06-18 22:48:22 +0200

    Update 3.ts

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,5 +1,7 @@
-function merge<T extends object, U extends object>(objA: T, objB: U) : T & U {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign({},objA, objB);
 }
+
+type mergedType = ReturnType<typeof merge>;
 
 export {};
